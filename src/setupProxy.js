7876104cbefbc14124828b2d1232dcd63d54d548
yuiTeikoku <msdoomay@gmail.com>
2019-10-16 16:02:55 +0300

    change api url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,12 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use('/api', proxy({
-    target: 'http://localhost:8000/',
+    target: 'https://yuiapi.herokuapp.com/',
     changeOrigin: true,
   }));
 
   app.use('/auth', proxy({
-    target: 'http://localhost:8000/',
+    target: 'https://yuiapi.herokuapp.com/',
     changeOrigin: true,
   }));
 };
